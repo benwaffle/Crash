72922dda14b0bee52d2330ed0e16a473d1287502
Ben Iofel <iofelben@gmail.com>
2014-05-23 20:27:12 -0600

    python

--- crash.java
+++ crash.java
@@ -1,7 +1,7 @@
 import java.lang.reflect.Field;
 import sun.misc.Unsafe;
 
-public class Crash {
+public class crash {
 	public static void main(String[] args) throws Exception {
 		Field privUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
 		privUnsafe.setAccessible(true);
