b009c6be6841d02f4d4856f882d3f8524027ee0d
benwaffle <benwaffle@users.noreply.github.com>
2015-01-04 04:19:50 -0500

    tiny java

--- crash.java
+++ crash.java
@@ -1,10 +1,5 @@
-import java.lang.reflect.Field;
-import sun.misc.Unsafe;
-
 public class crash {
-	public static void main(String[] args) throws Exception {
-		Field privUnsafe = Unsafe.class.getDeclaredField("theUnsafe");
-		privUnsafe.setAccessible(true);
-		((Unsafe) privUnsafe.get(null)).freeMemory(1);
+	public static void main(String... args) {
+		sun.nio.ch.NativeThread.signal(9);
 	}
 }
